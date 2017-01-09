ffc5252922a336481f873ecff69bac94d223845c
jaco <fbwotjq@gmail.com>
2017-01-09 22:46:59 +0900

    add

--- src/main/java/com/example/HomeController.java
+++ src/main/java/com/example/HomeController.java
@@ -8,6 +8,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.Protocol;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+
 @Controller
 public class HomeController {
 
@@ -17,6 +24,19 @@ public class HomeController {
 	@ResponseBody public Map<String, Object> home() {
 		Map<String, Object> map = new HashMap<>();
 		map.put("result", s3Wrapper.list());
+		return map;
+	}
+	
+	@GetMapping("/")
+	@ResponseBody public Map<String, Object> listBuckets() {
+		
+		AWSCredentials credentials = new BasicAWSCredentials("???", "???");
+        ClientConfiguration clientConfig = new ClientConfiguration();
+        clientConfig.setProtocol(Protocol.HTTP);
+        AmazonS3 conn = new AmazonS3Client(credentials, clientConfig);
+        conn.setEndpoint("s3-ap-northeast-2.amazonaws.com");
+		Map<String, Object> map = new HashMap<>();
+		map.put("result", conn.listBuckets());
 		return map;
 	}
 	
